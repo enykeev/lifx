47ea1b24c7a4c88f8f34c594089f3ae3389b6133
Kirill Enykeev <enykeev@mech.sh>
2021-11-05 19:50:53 +0300

    add timezone support

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import Lifx from 'node-lifx-lan';
 import scheduler from 'node-schedule';
 
+class Rule extends scheduler.RecurrenceRule {
+  constructor(opts) {
+    super();
+
+    for (const key in opts) {
+      this[key] = opts[key];
+    }
+  }
+}
+
 const filters = [{
   group: {label: 'Bedroom',}
 }];
@@ -9,34 +19,43 @@ const color = {css: 'white'};
 
 const duration = 5000;
 
-(async () => {
-  const deviceList = await Lifx.discover()
-  console.log(deviceList);
-
-  const onJob = scheduler.scheduleJob({ hour: 9, minute: 47 }, async () => {
+const jobs = [{
+  rule: new Rule({ hour: 19, minute: 46, tz: 'Europe/Moscow' }),
+  fn: async () => {
     console.log('switching lights on')
     await Lifx.turnOnFilter({
       filters,
       color,
       duration,
     });
-  });
-
-  const offJob = scheduler.scheduleJob({ hour: 9, minute: 49 }, async () => {
+  }
+}, {
+  rule: new Rule({ hour: 19, minute: 49, tz: 'Europe/Moscow' }),
+  fn: async () => {
     console.log('switching lights off')
     await Lifx.turnOffFilter({
       filters,
       color,
       duration,
     });
-  });
+  }
+}];
+
+(async () => {
+  const deviceList = await Lifx.discover()
+  console.log(deviceList.map(e => e.deviceInfo.label));
+
+  for (const { rule, fn } of jobs) {
+    scheduler.scheduleJob(rule, fn);
+  }
 
   [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`].forEach((eventType) => {
     process.on(eventType, () => {
       console.log('exiting');
       Lifx.destroy();
-      onJob.cancel();
-      offJob.cancel();
+      for (const job of Object.values(scheduler.scheduledJobs)) {
+        job.cancel()
+      }
     });
   });
 })()
